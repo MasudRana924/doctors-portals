5a6d2c18b673090da4b6a52ceeb530ed1bd56186
MasudRana924 <masud15-924@diu.edu.bd>
2021-11-04 10:34:45 -0600

    modal

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ import {
   Link
 } from "react-router-dom";
 import Home from './Pages/Home/Home';
+import Appointment from './Pages/Appointment/Appointment';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
+        <Route  path="/appointment">
+             <Appointment></Appointment>
+          </Route>
           <Route exact path="/">
               <Home></Home>
           </Route>
